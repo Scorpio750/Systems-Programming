efc9f486ea43b4dc04ea559c5da3298eaa9671bd
alisonwong <allywong2@gmail.com>
2014-10-21 16:52:33 -0400

    i<3alison

--- pa3/indexer.c
+++ pa3/indexer.c
@@ -1,11 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <dirent.h>
-#include <ctype.h>
-#include <sys/stat.h>
 #include "indexer.h"
 
 /* Hash Table functions */
@@ -16,6 +8,118 @@ Hash_Table * createTable () {
 	return table;
 }
 
+Prefix_Node *createNode(char c, char *filename){
+	TNode *node = (Prefix_Node*)(calloc(1, sizeof(Prefix_Node)));
+	node->c = c;
+	node->depth = 0;
+	node->isWord = false;
+	node->head = NULL;
+	node->next = (Prefix_Node**)(calloc(36, sizeof(Prefix_Node)));
+	return node;
+}
+
+File_Node *createFileNode(Prefix_Node *node, char *filename){
+	File_Node *node = (File_node*)calloc((1, sizeof(File_Node)));
+	node->filename = filename;
+	node->occurrences = 1;
+	node->next = NULL;
+	node->prev = NULL;
+}
+
+void swap(Prefix_Node *node, File_Node *small, File_Node *big){
+	if(small == node->head){
+		small->next = big->next;
+		small->prev = big;
+		big->prev = NULL
+		big->next = small;
+		node->head = big;
+		return;
+	}
+
+	small->next = big->next;
+	big->prev = small->prev;
+	small->prev->next = big;
+	small->prev = big;
+	big->next = small;
+
+	if(small->next != NULL)
+		small->next->prev = small;
+
+	File_Node *prev;
+	if(big->prev != NULL){
+		prev = big->prev->occurrences;
+		if(prev < big->occurrences){
+			swap(node, prev, big);
+		}
+	}
+	return;
+}
+
+void list(Prefix_Node *node, char *filename){
+	File_Node *fileNode;
+	int prevOcc;
+	if (node->head == NULL){
+		fileNode = createFileNode(fileNode, filename);
+		node->head = fileNode;
+		return;
+	}
+
+	for(fileNode = node->head; fileNode != NULL; fileNode = fileNode->next){
+		if(fileNode->filename == filename){
+			fileNode->occurrences++;
+			if(fileNode->prev != NULL){
+				prevOcc = fileNode->prev->occurrences;
+				if(prevOcc < fileNode->occurrences)
+					swap(node, fileNode->prev, fileNode);
+			}
+			return;
+		}
+		if(fileNode->next == NULL){
+			File_Node *newNode = createFileNode(fileNode, filename);
+			fileNode->next = newNode;
+			newNode->prev = fileNode;
+			newNode->next = NULL;
+		}
+	}
+}
+
+int hash(char c) {
+	char * index = strchr(acceptable, c);
+	if (index == NULL) {
+		return -1;
+	}
+	return (int)(index - acceptable);
+}
+
+void insertTrie(FILE *file, Hash_Table *table, char *filename){
+	if(table->head == NULL)
+		table->head = createNode(' ');
+
+	Prefix_Node *ptr = table->head;
+	int index;
+	int c = tolower(fgetc(file)); //can you tolower a digit?
+
+	while (c != EOF){
+		if (isalpha(c) || isdigit(c)){
+			index = hash(c);
+			if (ptr->children[index] == NULL){
+				ptr->children[index] = createNode(c);
+				ptr->children[index]->depth  = ptr->depth+1;
+			}
+			ptr = ptr->children[index];
+		}
+		if ((!isalpha(c)) && (!isdigit(c)) && ptr != root){
+			ptr->isWord = true;
+			if(ptr->isWord)
+				list(ptr,filename);
+			ptr = table->head;
+		}
+		c = tolower(fgetc(file)); //can you to lower a digit?
+	}
+	return;
+}
+
+/*
 void insertHashTable(Hash_Table * table, char c, char * filename) {
 	File_Node * fileptr = NULL;
 	int index = 0;
@@ -42,14 +146,8 @@ void insertHashTable(Hash_Table * table, char c, char * filename) {
 		}
 	}
 }
+/*
 
-int hash(char c) {
-	char * index = strchr(acceptable, c);
-	if (index == NULL) {
-		return -1;
-	}
-	return (int)(index - acceptable);
-}
 
 /* File System functions */
 int checkDir(char * file) {
@@ -148,7 +246,7 @@ void recursivePrintTree(char *buffer, Prefix_Node * ptr, FILE *file){
 	for (int i = 35; i > 0; i--) {
 		if (ptr->next[i] == NULL)
 			continue;
-		index = ptr->next[i]->depth;
+		index = ptr->next[i]->depth-1;
 		buffer[index] = (char)ptr->next[i]->c;
 		if(ptr->next[i]->isWord){
 			buffer[index+1] = '\0';
@@ -162,8 +260,6 @@ void printTree(Prefix_Node * head, FILE *file) {
 	char buffer[256];
 	Prefix_Node * headptr = head;
 	recursivePrintTree(buffer, headptr, file);
-
-
 }
 
 void dump_to_file(Hash_Table * inv_index, char * filename) {
